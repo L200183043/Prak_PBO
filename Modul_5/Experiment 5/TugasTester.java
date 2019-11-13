1ee88b6bc52c9b0c239be920c66736d098ada7ea
L200183043 <45007772+L200183043@users.noreply.github.com>
2019-11-13 23:20:43 +0700

    Add files via upload

---
+++ Modul_5/Experiment 5/TugasTester.java
@@ -0,0 +1,6 @@
+public class TugasTester {
+    public static void main(String[] args) {
+        Tugas tugas1 = new Tugas();
+        Tugas tugas2 = new Tugas("Aria Widio", "20 November 1999");
+    }
+}
